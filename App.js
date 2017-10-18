540c7927cd8e0e579712e1b4ec652aaacb9db444
Konstantin Shkut <Konstantin.Shkut@gmail.com>
2017-10-18 14:08:58 -0700

    setting up the basics

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// @flow
 
-export default class App extends React.Component {
+import React, { PureComponent } from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default class Main extends PureComponent {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <StatusBar hidden={true} />
+
+        <View style={styles.header}>
+          <Text style={[styles.text, styles.title]}>
+            Let's drag and drop some tags!
+          </Text>
+          <Text style={styles.text}>
+            Drag and drop tags to reorder, tap to remove or press Add New to add new tags.
+          </Text>
+        </View>
+
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#2196F3',
+  },
+  header: {
+    marginHorizontal: 20,
+    marginVertical: 50,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    color: '#FFFFFF',
+    fontFamily: 'Avenir',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
