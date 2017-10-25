920c7fb6771184d4dbce5bdf5e6688216837fd6c
Konstantin Shkut <Konstantin.Shkut@gmail.com>
2017-10-25 08:05:12 -0700

    add ability to remove tags when tapping on them

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -2,6 +2,7 @@
 
 import React, { PureComponent } from 'react';
 import {
+  LayoutAnimation,
   StyleSheet,
   View
 } from 'react-native';
@@ -11,6 +12,8 @@ import type { TagObject } from '../types';
 type Props = {
   // Array of tag titles
   tags: string[],
+  // Tag swapping animation duration in ms
+  animationDuration: number,
   // Passes onPressAddNewTag callback down to TagsArea component
   onPressAddNewTag: () => void,
 };
@@ -23,11 +26,37 @@ export default class Tags extends PureComponent {
 
   props: Props;
 
+  static defaultProps = {
+    animationDuration: 250
+  };
+
   state: State = {
     // Convert passed array of tag titles to array of objects of TagObject type,
     // so ['tag', 'another'] becomes [{ title: 'tag' }, { title: 'another' }]
     tags: [...new Set(this.props.tags)]       // remove duplicates
       .map((title: string) => ({ title })),   // convert to objects
+  };
+
+  // Animate layout changes when dragging or removing a tag
+  componentWillUpdate() {
+    LayoutAnimation.configureNext({
+      ...LayoutAnimation.Presets.easeInEaseOut,
+      duration: this.props.animationDuration
+    });
+  }
+
+  // Remove tag
+  removeTag = (tag: TagObject): void => {
+    this.setState((state: State) => {
+      const index = state.tags.findIndex(({ title }) => title === tag.title);
+      return {
+        tags: [
+          // Remove the tag
+          ...state.tags.slice(0, index),
+          ...state.tags.slice(index + 1),
+        ]
+      }
+    });
   };
 
   render() {
@@ -39,7 +68,7 @@ export default class Tags extends PureComponent {
 
         <TagsArea
           tags={tags}
-          onPress={() => {}} // do nothing for now
+          onPress={this.removeTag}
           onRenderTag={() => {}} // do nothing for now
           onPressAddNew={this.props.onPressAddNewTag}
         />
